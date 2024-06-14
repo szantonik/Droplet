0836195b5a326392e7008fad9adce3e08851aa9c
szantonik <szantonik@gmail.com>
2024-06-14 13:36:32 +0200

    Dodaj pliki projektów.

---
+++ Droplet/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    var currentPath = window.location.pathname;
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function (link) {
+        var linkPath = link.getAttribute('href');
+        if (linkPath && currentPath.includes(linkPath)) {
+            link.classList.add('active');
+        }
+    });
+});
